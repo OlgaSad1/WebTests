86b55cca419dc7fd2d00927e1e34411846b5ed48
Olga Sad <mrs.sadeghi@mail.ru>
2021-10-10 20:08:58 -0500

    WebTests project's first commit

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.imageio.plugins.tiff.TIFFImageReadParam;
+
+public class WebTest {
+    @Test
+    public void testFirst() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Drivers\\chromedriver.exe";
+        String url =  "http://www.99-bottles-of-beer.net/";
+        String expectedResult ="http://www.99-bottles-of-beer.net/";
+
+
+
+        System.setProperty(chromeDriver, driverPath);
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, expectedResult);
+
+
+
+        driver.close();
+        driver.quit();
+
+    }
+
+
+}
